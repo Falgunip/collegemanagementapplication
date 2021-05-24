a81ace5decc53b615257ea1db112a54c35bb22d3
Falgunip <63282954+Falgunip@users.noreply.github.com>
2021-05-24 14:12:15 -0700

    Update settings.py

--- collegemanagementsystem/settings.py
+++ collegemanagementsystem/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bz9hj=3oa6_5pndqg+=9s@nf3)gea(l78x+-8(zm9#m#(dqmfr'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
